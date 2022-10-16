73af107585c9b553dde1cd321b8d7aa27230bd2e
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-16 16:11:39 +0200

    done with phase 3 in progress

---
+++ server/db/models/insect.js
@@ -0,0 +1,42 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class Insect extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Insect.init(
+		{
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true,
+				validate: {
+					isTitle(value) {
+						const initials = value.split(" ").map((word) => word.split("")[0]);
+						initials.forEach((element) => {
+							if (element.toUpperCase !== element) {
+								throw new Error("Must be Title Cased.. ");
+							}
+						});
+					},
+				},
+			},
+			description: DataTypes.STRING,
+			territory: DataTypes.STRING,
+			fact: DataTypes.STRING,
+			millimeters: DataTypes.FLOAT,
+		},
+		{
+			sequelize,
+			modelName: "Insect",
+		}
+	);
+	return Insect;
+};
